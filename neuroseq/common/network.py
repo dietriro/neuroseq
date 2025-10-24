3f7f30e248fb83989bfa982f9deb116cc888feee
Robin Dietrich <robin.dietrich@tum.de>
2025-10-24 14:44:14 +1000

    Fixed inh-to-exc-global connections

--- neuroseq/common/network.py
+++ neuroseq/common/network.py
@@ -365,16 +365,13 @@ class SHTMBase(ABC):
                 receptor_type=self.p.synapses.receptor_exc_inh))
 
         self.inh_to_exc_global = []
-        for i in range(self.p.network.num_columns):
-            inh_to_exc_global_i = list()
-            for k_col in range(self.p.network.num_columns):
-                inh_to_exc_global_i.append(Projection(
-                    self.neurons_inh_global,
-                    self.get_neurons(NeuronType.Soma, column_id=k_col),
-                    AllToAllConnector(),
-                    synapse_type=StaticSynapse(weight=self.p.synapses.w_inhg_exc, delay=0.1),
-                    receptor_type=self.p.synapses.receptor_inh_exc))
-            self.inh_to_exc_global.append(inh_to_exc_global_i)
+        for i_col in range(self.p.network.num_columns):
+            self.inh_to_exc_global.append(Projection(
+                self.neurons_inh_global,
+                self.get_neurons(NeuronType.Soma, column_id=i_col),
+                AllToAllConnector(),
+                synapse_type=StaticSynapse(weight=self.p.synapses.w_inhg_exc, delay=0.1),
+                receptor_type=self.p.synapses.receptor_inh_exc))
 
     def init_plasticity_rule(self):
         pass
